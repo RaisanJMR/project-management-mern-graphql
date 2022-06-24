930234bee09380412fb1923420d2e5bf418c8c65
RaisanJMR <raisanjmr@protonmail.com>
2022-06-24 21:22:23 +0530

    added graphql schema

--- server/index.js
+++ server/index.js
@@ -2,13 +2,19 @@ const express = require('express')
 require("dotenv").config();
 const { graphqlHTTP } = require('express-graphql')
 const schema = require('./schema/schema')
+
 const port = process.env.PORT || 5000;
 
-const app = express();
+const app = express()
+
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
 
 app.use('/graphql', graphqlHTTP({
     schema,
-    graphiql: process.env.NODE_ENV === 'developemnt'
+    graphiql: process.env.NODE_ENV === 'development'
 }))
 
 app.listen(port, console.log(`Server running on port ${port}`))
