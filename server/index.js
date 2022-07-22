8baa535f4b7a9664b81e8e92eaf3902e4f89a989
RaisanJMR <raisanjmr@protonmail.com>
2022-07-22 16:09:40 +0530

    added mongoDB

--- server/index.js
+++ server/index.js
@@ -2,12 +2,12 @@ const express = require('express')
 require("dotenv").config();
 const { graphqlHTTP } = require('express-graphql')
 const schema = require('./schema/schema')
-
+const colors = require('colors');
 const port = process.env.PORT || 5000;
-
+const connectDB = require('./config/db')
 const app = express() 
 
-
+connectDB();
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
@@ -17,4 +17,4 @@ app.use('/graphql', graphqlHTTP({
     graphiql: process.env.NODE_ENV === 'development'
 }))
 
-app.listen(port, console.log(`Server running on port ${port}`))
+app.listen(port, console.log(`Server running on port ${port}`.yellow.bold))
